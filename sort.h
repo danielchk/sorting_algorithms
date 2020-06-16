9d9de30ce4db6b5f60f53394199f006f1089986b
danielchk <hollowdaniel1@gmail.com>
2020-06-16 18:34:08 +0000

    fix 1

--- sort.h
+++ sort.h
@@ -23,6 +23,5 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void nodes_switching(listint_t *node1, listint_t *node2);
 
 #endif
